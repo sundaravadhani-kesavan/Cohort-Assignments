3fd0479cb13be971fdcc519d6f118c5dd9a03e55
sundaravadhani <sundaravadhanikesavan@gmail.com>
2025-06-25 12:34:01 +0530

    day 25 changes

--- day 25/app.js
+++ day 25/app.js
@@ -1,24 +1,7 @@
-//promise syntax
-
-// const newPromise=new Promise((res,rej)=>{
-//      rej("Task is not completed")
-// }).then((x)=>console.log(x)).catch((y)=>console.log(y))
-
-
-// fetch("https://api.quotable.io/quotes?limit=100").then((res)=>{
-//      return res.json()
-// }).then((data)=>{
-//      console.log(data);
-     
-// }) 
-
 async function showquotes(){
      let data=await fetch("https://dummyjson.com/quotes?limit=100")
-     // console.log(data);
      let response=await data.json()
-     // console.log(response);
      let quotesList=response.quotes
-     // console.log(quotesList);
      let random=Math.floor(Math.random()*quotesList.length)
      let showList=quotesList[random]
      console.log(showList);
@@ -29,17 +12,10 @@ showquotes()
 
 async function showProfile() {
      let data=await fetch("https://randomuser.me/api")
-     // console.log(data);
      let response=await data.json()
-     // console.log(response);
      let profileList=response.results[0]
      console.log(profileList);
-
-     let showimg=
-          `
-          <img src="${profileList.picture.large}"><br>
-          `
-
+     let showimg=`<img src="${profileList.picture.large}"><br>`
      let profileDetails= `
           Name: ${profileList.name.first} & ${profileList.name.last}<br>
           Gender: ${profileList.gender}<br>
@@ -49,12 +25,8 @@ async function showProfile() {
           City : ${profileList.location.city}<br>
           Country : ${profileList.location.country}<br>
           Username : ${profileList.login.username}<br>
-          Password : ${profileList.login.password}
-          <br>`
+          Password : ${profileList.login.password}<br>`
     document.getElementById("showimg").innerHTML = showimg;
-    document.getElementById("show").innerHTML = profileDetails;
-
-
-     
+    document.getElementById("show").innerHTML = profileDetails;     
 }
 showProfile()
